f5abe526fe3da760aa7b33570c1e75336df14164
Gyztor Mizirath <44737827+Gyztor@users.noreply.github.com>
2022-05-20 19:47:57 -0400

    test

--- root/app/functions/check-mount.sh
+++ root/app/functions/check-mount.sh
@@ -2,19 +2,19 @@
 set -euf
 
 # Ensure that /data is mounted with execute permissions
-MOUNT_OUTPUT=$(mount | grep " /data ")
+MOUNT_OUTPUT=$(mount | grep " /home/container ")
 case "${MOUNT_OUTPUT}" in
 *noexec*)
     echo ""
     echo "ERROR"
-    echo "Your /data mount does not allow execution, as the \"noexec\" flag is present:"
+    echo "Your /home/container mount does not allow execution, as the \"noexec\" flag is present:"
     echo ""
     echo "${MOUNT_OUTPUT}"
     echo ""
     echo "In order for this container to work correctly, either remove \"noexec\""
-    echo "from the host mount options that /data is using (usually by editing"
+    echo "from the host mount options that /home/container is using (usually by editing"
     echo "the hosts /etc/fstab file), or choose a different location on the host"
-    echo "for the /data volume that allows execution."
+    echo "for the /home/container volume that allows execution."
     exit 1
     ;;
 esac
