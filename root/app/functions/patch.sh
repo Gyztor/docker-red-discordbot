4f292eb0378d1a46312f9bcca7034ce21ef437bb
Gyztor Mizirath <44737827+Gyztor@users.noreply.github.com>
2022-05-20 19:49:17 -0400

    test

--- root/app/functions/patch.sh
+++ root/app/functions/patch.sh
@@ -2,15 +2,15 @@
 set -euf
 
 # Python 3.8, remove mongo dependencies
-if ! [ -f "/data/venv/.pcxversion" ]; then
-    rm -rf /data/venv
-    mkdir -p /data/venv
-    echo "2" >"/data/venv/.pcxversion"
+if ! [ -f "/home/container/venv/.pcxversion" ]; then
+    rm -rf /home/container/venv
+    mkdir -p /home/container/venv
+    echo "2" >"/home/container/venv/.pcxversion"
 fi
 
 # Python 3.9, remove 3.8 dependencies
-if [ "$(cat "/data/venv/.pcxversion")" = "1" ]; then
-    rm -rf /data/venv
-    mkdir -p /data/venv
-    echo "2" >"/data/venv/.pcxversion"
+if [ "$(cat "/home/container/venv/.pcxversion")" = "1" ]; then
+    rm -rf /home/container/venv
+    mkdir -p /home/container/venv
+    echo "2" >"/home/container/venv/.pcxversion"
 fi
