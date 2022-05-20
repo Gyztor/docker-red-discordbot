05e80a1134557068dfb2b4416fe08816cb1e5a9d
Gyztor Mizirath <44737827+Gyztor@users.noreply.github.com>
2022-05-20 19:50:43 -0400

    Update setup-env.sh

--- root/app/functions/setup-env.sh
+++ root/app/functions/setup-env.sh
@@ -5,27 +5,27 @@ set -euf
 /app/functions/patch.sh
 
 # If config file doesn't exist, make one
-if ! [ -f "/data/config.json" ]; then
+if ! [ -f "/home/container/config.json" ]; then
     if [ -f "${HOME}/config.json" ]; then
         # Migrating old data
-        echo "Moving ${HOME}/config.json to /data/config.json"
-        mv "${HOME}/config.json" /data/config.json
+        echo "Moving ${HOME}/config.json to /home/container/config.json"
+        mv "${HOME}/config.json" /home/container/config.json
     else
         # Default to JSON storage
-        cp /defaults/config.json /data/config.json
+        cp /defaults/config.json /home/container/config.json
     fi
 fi
 
 # If config symlink is broken because user mounted the home directory (/config or /root), make it
-if [ "$(readlink -f "${HOME}/.config/Red-DiscordBot/config.json")" != "/data/config.json" ]; then
+if [ "$(readlink -f "${HOME}/.config/Red-DiscordBot/config.json")" != "/home/container/config.json" ]; then
     rm -rf "${HOME}/.config/Red-DiscordBot/config.json"
     mkdir -p "${HOME}/.config/Red-DiscordBot"
-    ln -s /data/config.json "${HOME}/.config/Red-DiscordBot/config.json"
+    ln -s /home/container/config.json "${HOME}/.config/Red-DiscordBot/config.json"
 fi
 
 # Prepare and activate venv
 echo "Activating Python virtual environment..."
-mkdir -p /data/venv
-python -m venv --upgrade --upgrade-deps /data/venv
-python -m venv /data/venv
-. /data/venv/bin/activate
+mkdir -p /home/container/venv
+python -m venv --upgrade --upgrade-deps /home/container/venv
+python -m venv /home/container/venv
+. /home/container/venv/bin/activate
